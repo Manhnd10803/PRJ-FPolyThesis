5407b4010e594ffa834dd3bcf9e64b5ac66b898c
mhfed <nmhieu04091999@gmail.com>
2023-10-04 22:22:06 +0700

    fix: remove __dirname

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,6 @@ import laravel from 'laravel-vite-plugin';
 import react from '@vitejs/plugin-react';
 import path from 'path';
 
-const __dirname = path.dirname(new URL(import.meta.url).pathname);
-
 export default defineConfig({
   plugins: [
     laravel({
