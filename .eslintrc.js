aef25541ec05f13b4cc52c463bcf068b3b1ed6bb
mhfed <nmhieu04091999@gmail.com>
2023-10-11 02:01:41 +0700

    Setup Prettier & Remove unused package

---
+++ .eslintrc.js
@@ -0,0 +1,71 @@
+module.exports = {
+  root: true,
+  parserOptions: {
+    project: './tsconfig.json',
+    ecmaVersion: 2020, // Use the latest ecmascript standard
+    sourceType: 'module', // Allows using import/export statements
+    ecmaFeatures: {
+      jsx: true, // Enable JSX since we're using React
+    },
+  },
+  settings: {
+    'import/resolver': {
+      typescript: {
+        alwaysTryTypes: true,
+      },
+    },
+    react: {
+      version: 'detect',
+    },
+  },
+  env: {
+    browser: true,
+    amd: true,
+    node: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:react-hooks/recommended',
+    'plugin:prettier/recommended',
+  ],
+  plugins: ['prettier', '@typescript-eslint'],
+  rules: {
+    'object-curly-spacing': ['warn', 'always'],
+
+    'react/jsx-first-prop-new-line': [2, 'multiline'],
+    'react/jsx-max-props-per-line': [2, { maximum: 1, when: 'multiline' }],
+    'react/jsx-indent-props': [2, 2],
+    'react/jsx-closing-bracket-location': [2, 'tag-aligned'],
+    'prettier/prettier': [
+      'error',
+      {},
+      {
+        usePrettierrc: true,
+      },
+    ],
+    'react/react-in-jsx-scope': 'off',
+    'react/prop-types': 'off',
+    'object-curly-spacing': ['warn', 'always'],
+    'max-len': 'off',
+    'no-plusplus': [
+      'error',
+      {
+        allowForLoopAfterthoughts: true,
+      },
+    ],
+
+    'no-console': 2,
+
+    'no-unused-vars': 'warn',
+    'react/jsx-key': 'error',
+    'react-hooks/exhaustive-deps': 'warn',
+
+    'import/no-extraneous-dependencies': [
+      'error',
+      {
+        devDependencies: ['**/*.test.js', '**/*.test.jsx', '**/*.test.ts', '**/*.test.tsx', 'src/tests/**/*'],
+      },
+    ],
+  },
+};
