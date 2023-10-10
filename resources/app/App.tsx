01e4b7eb2b6ff6c8e829731aaad04072ca3e6103
mhfed <nmhieu04091999@gmail.com>
2023-10-10 20:45:13 +0700

    refactor: remove old logic

--- resources/app/App.tsx
+++ resources/app/App.tsx
@@ -1,17 +1,9 @@
 import './bootstrap';
 
-import Router from '@/routes/Routes';
-import { BrowserRouter } from 'react-router-dom';
-import { ThemeProvider } from './components/provider';
-
 function App() {
   return (
     <>
-      <ThemeProvider defaultTheme='dark' storageKey='vite-ui-theme'>
-        <BrowserRouter>
-          <Router />
-        </BrowserRouter>
-      </ThemeProvider>
+      <h1>Hello</h1>
     </>
   );
 }
--- resources/app/components/guard/AuthGuard.tsx
+++ resources/app/components/guard/AuthGuard.tsx
@@ -1,18 +1,7 @@
 import React, { FC } from 'react';
-// import { Navigate } from 'react-router-dom';
-// import { PATH_NAME } from '@/routes/pathName';
-
 type AuthGuardProps = {
   children?: React.ReactNode;
 };
 export const AuthGuard: FC<AuthGuardProps> = ({ children }) => {
-  // selector get isAuth
-
-  // if not login or not auto login navigate to login page
-  // if (!isAuth) {
-  //   return <Navigate to={PATH_NAME.LOGIN} />;
-  // }
-
-  // else show router
   return <>{children}</>;
 };
