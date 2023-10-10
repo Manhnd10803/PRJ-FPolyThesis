69b5a7049b11a8d7a78876b4fe2e285ce586780f
mhfed <nmhieu04091999@gmail.com>
2023-10-11 00:52:22 +0700

    feat: client layout component

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -7,14 +7,16 @@
         <title>Laravel</title>
 
         <!-- Fonts -->
-        <link rel="preconnect" href="https://fonts.bunny.net">
-        <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
+        <!-- <link rel="preconnect" href="https://fonts.bunny.net"> -->
+        <!-- <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" /> -->
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
 
         
         @viteReactRefresh
         @vite(['resources/app/index.tsx'])
     </head>
     <body class="min-h-screen bg-background font-sans antialiased">
+        <noscript>You need to enable JavaScript to run this app.</noscript>
         <div id="app"></div>
     </body>
 </html>
