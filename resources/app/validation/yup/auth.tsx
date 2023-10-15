c670e12826d7a469d182f35c179bc6228d7377ff
Phan Luan <phanluandev@gmail.com>
2023-10-15 10:39:21 +0700

    feat: validate login form

---
+++ resources/app/validation/yup/auth.tsx
@@ -0,0 +1,8 @@
+import * as Yup from 'yup';
+
+const LoginShema = Yup.object().shape({
+  email: Yup.string().email('Invalid email address').required('Email is required'),
+  password: Yup.string().min(6, 'Password must be at least 6 characters').required('Password is required'),
+});
+
+export { LoginShema };
