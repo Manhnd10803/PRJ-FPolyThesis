7c99715d15b5b321af11a85142312a2aa5b2cf62
Phan Luan <phanluandev@gmail.com>
2023-10-16 09:18:42 +0700

    refactor: Login with Google

--- resources/app/apis/auth.tsx
+++ resources/app/apis/auth.tsx
@@ -7,4 +7,24 @@ const Login = (data: any) => {
 const Register = (data: any) => {
   return instance.post('register', data);
 };
-export { Login, Register };
+
+const SigninWithGoogle = () => {
+  fetch('http://localhost:8000/api/google-auth', {
+    mode: 'no-cors',
+  })
+    .then(response => {
+      if (response.ok) {
+        return response.json();
+      }
+      throw new Error('Something went wrong!');
+    })
+    .then(({ url }) => (window.location.href = url))
+    .then(response => {
+      console.log(response);
+    })
+    .catch(error => {
+      console.error('Error:', error);
+    });
+};
+
+export { Login, Register, SigninWithGoogle };
