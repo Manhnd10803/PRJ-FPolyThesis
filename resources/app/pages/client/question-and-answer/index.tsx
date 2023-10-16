4d5988546ff6c7bf12a9196140d844db3b7a0cf0
dammebatdiet0505 <dammebatdiet0505@gmail.com>
2023-10-16 22:13:34 +0700

    feat: DTNFP-94 add quenstion and answer

--- resources/app/pages/client/question-and-answer/index.tsx
+++ resources/app/pages/client/question-and-answer/index.tsx
@@ -1,12 +1,25 @@
-import { Container } from 'react-bootstrap';
+import { Container, Col, Row, Card, Button, Nav, Modal } from 'react-bootstrap';
+import { Link, useLocation } from 'react-router-dom';
+import { ListQandAPage } from './components/list-qanda';
+import { FormAskQuestionPage } from './components/form-ask-question';
 
 export const QuestionAndAnswerPage = () => {
+  const location = useLocation();
+  const routeComponents = {
+    '/question-and-answer': <ListQandAPage />,
+    '/form-ask-question': <FormAskQuestionPage />,
+  };
+  const selectedComponent = routeComponents[location.pathname] || null;
   return (
     <>
       <div id="content-page" className="content-page">
-        <Container>
-          <h1>QuestionAndAnswerPage</h1>
-        </Container>
+        {selectedComponent}
+
+        {/* {location.pathname === '/question-and-answer' ? (
+          <ListQandAPage />
+        ) : location.pathname === '/form-ask-question' ? (
+          <FormAskQuestionPage />
+        ) : null} */}
       </div>
     </>
   );
