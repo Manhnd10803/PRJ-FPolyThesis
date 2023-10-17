16de74649d20dcdb2f18e81c62d6bf2e3283489f
Manhnd10803 <118704220+Manhnd10803@users.noreply.github.com>
2023-10-17 11:44:07 +0700

    build: swagger api

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -6,6 +6,9 @@ use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
 
+/**
+ * @OA\Info(title="PRJ - FpolyThesis", version="1.0")
+ */
 class Controller extends BaseController
 {
     use AuthorizesRequests, ValidatesRequests;
