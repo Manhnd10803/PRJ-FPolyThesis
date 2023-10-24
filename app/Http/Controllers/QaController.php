7d15a341f683182ac1f22c5a5b5db6db37d3d155
linh241103 <koco19112002@gmail.com>
2023-10-24 13:44:18 +0700

    feat: Swagger QaController

--- app/Http/Controllers/QaController.php
+++ app/Http/Controllers/QaController.php
@@ -10,6 +10,27 @@ use Illuminate\Support\Facades\DB;
 
 class QaController extends Controller
 {
+    /**
+     * @OA\Post(
+     *     path="/api/quests",
+     *     tags={"Q&A"},
+     *     summary="Tạo câu hỏi mới",
+     *     description="Tạo một câu hỏi mới với tiêu đề, nội dung, chuyên ngành liên quan và hashtag.",
+     *     @OA\RequestBody(
+     *         required=true,
+     *         @OA\JsonContent(
+     *             required={"title", "content", "majors_id", "hashtag"},
+     *             @OA\Property(property="title", type="string", description="Tiêu đề của câu hỏi", maxLength=255),
+     *             @OA\Property(property="content", type="string", description="Nội dung của câu hỏi"),
+     *             @OA\Property(property="majors_id", type="integer", description="ID của chuyên ngành liên quan đến câu hỏi"),
+     *             @OA\Property(property="hashtag", type="string", description="Hashtag của câu hỏi", maxLength=255)
+     *         )
+     *     ),
+     *     @OA\Response(response=200, description="Câu hỏi đã được tạo thành công"),
+     *     @OA\Response(response=400, description="Lỗi khi tạo câu hỏi")
+     * )
+     */
+
   public function CreateQa(Request $request){
     DB::beginTransaction();
     try{
@@ -30,6 +51,35 @@ class QaController extends Controller
         return response()->json(['errors' => $e->getMessage()], 400);
     }
   }
+    /**
+     * @OA\Put(
+     *     path="/api/quests/{qa}",
+     *     tags={"Q&A"},
+     *     summary="Cập nhật câu hỏi",
+     *     description="Cập nhật một câu hỏi với tiêu đề, nội dung, chuyên ngành liên quan và hashtag.",
+     *     @OA\Parameter(
+     *         name="qa",
+     *         in="path",
+     *         required=true,
+     *         description="ID của câu hỏi cần cập nhật",
+     *         @OA\Schema(type="integer")
+     *     ),
+     *     @OA\RequestBody(
+     *         required=true,
+     *         @OA\JsonContent(
+     *             required={"title", "content", "majors_id", "hashtag"},
+     *             @OA\Property(property="title", type="string", description="Tiêu đề của câu hỏi", maxLength=255),
+     *             @OA\Property(property="content", type="string", description="Nội dung của câu hỏi"),
+     *             @OA\Property(property="majors_id", type="integer", description="ID của chuyên ngành liên quan đến câu hỏi"),
+     *             @OA\Property(property="hashtag", type="string", description="Hashtag của câu hỏi", maxLength=255)
+     *         )
+     *     ),
+     *     @OA\Response(response=200, description="Câu hỏi đã được cập nhật thành công"),
+     *     @OA\Response(response=400, description="Lỗi khi cập nhật câu hỏi"),
+     *     @OA\Response(response=404, description="Câu hỏi không được tìm thấy")
+     * )
+     */
+
 public function UpdateQa(Request $request, Qa $qa) {
     DB::beginTransaction();
     try {
@@ -48,6 +98,26 @@ public function UpdateQa(Request $request, Qa $qa) {
         return response()->json(['errors' => $e->getMessage()], 400);
     }
 }
+    /**
+     * @OA\Delete(
+     *     path="/api/quests/{qa}",
+     *     tags={"Q&A"},
+     *     summary="Xóa câu hỏi và câu trả lời",
+     *     description="Xóa một câu hỏi và tất cả các câu trả lời liên quan dựa trên ID.",
+     *     @OA\Parameter(
+     *         name="qa",
+     *         in="path",
+     *         required=true,
+     *         description="ID của câu hỏi cần xóa",
+     *         @OA\Schema(type="integer")
+     *     ),
+     *     @OA\Response(response=200, description="Câu hỏi và câu trả lời đã bị xóa thành công"),
+     *     @OA\Response(response=400, description="Lỗi khi xóa câu hỏi và câu trả lời"),
+     *     @OA\Response(response=401, description="Không được phép xóa câu hỏi và câu trả lời"),
+     *     @OA\Response(response=404, description="Câu hỏi không được tìm thấy")
+     * )
+     */
+
 public function DeleteQa(Qa $qa){
     DB::beginTransaction();
     try {
