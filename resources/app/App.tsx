4af082d0ff4c84f4eb1fcea184307c5d5ef24280
mhfed <nmhieu04091999@gmail.com>
2023-10-31 11:45:33 +0700

    fix:  lexical editor

--- resources/app/App.tsx
+++ resources/app/App.tsx
@@ -4,12 +4,13 @@ import './assets/scss/index.scss';
 import './assets/scss/customizer.scss';
 import { useDispatch } from 'react-redux';
 import { setSetting } from './redux/slice';
+import { ErrorBoundary } from './ErrorBoundary';
 
 function App(props: { children: React.ReactNode }) {
   const dispatch = useDispatch();
   dispatch(setSetting());
 
-  return <>{props.children}</>;
+  return <ErrorBoundary>{props.children}</ErrorBoundary>;
 }
 
 export default App;
