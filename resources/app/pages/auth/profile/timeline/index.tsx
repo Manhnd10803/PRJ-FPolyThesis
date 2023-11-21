c729b43b77c8aa2b632b8fdc2b02efba07b26624
Nguyen Thanh Tung <tung19022003@gmail.com>
2023-11-21 15:14:24 +0700

    fix: scroll transform for profile timeline

--- resources/app/pages/auth/profile/timeline/index.tsx
+++ resources/app/pages/auth/profile/timeline/index.tsx
@@ -1,25 +1,47 @@
+import React, { useState, useEffect } from 'react';
 import { Card, Col, Row } from 'react-bootstrap';
 import { CreatePost } from './components/create-post';
 import { ListPost } from './list-post';
 import { About } from './about';
 import MyPhoto from './my-photo';
 import { FriendList } from './friend-list';
+import './index.css';
+
 export const Timeline = ({ about, listPost, isLoading, friend_id }) => {
+  const [isSticky, setSticky] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const offset = window.scrollY;
+      const topThreshold = 50;
+
+      if (offset >= topThreshold) {
+        setSticky(true);
+      } else {
+        setSticky(false);
+      }
+    };
+
+    window.addEventListener('scroll', handleScroll);
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
+
   return (
-    <>
-      <Card.Body className=" p-0">
-        <Row>
-          <Col lg={4}>
-            <About aboutUser={about} isLoading={isLoading} />
-            <MyPhoto />
-            <FriendList />
-          </Col>
-          <Col lg={8}>
-            {!friend_id ? <CreatePost /> : ''}
-            <ListPost listPost={listPost} isLoading={isLoading} />
-          </Col>
-        </Row>
-      </Card.Body>
-    </>
+    <Card.Body className="p-0">
+      <Row>
+        <Col lg={4} className={`col-4 ${isSticky ? 'sticky-col-4' : ''}`}>
+          <About aboutUser={about} isLoading={isLoading} />
+          <MyPhoto />
+          <FriendList />
+        </Col>
+        <Col lg={8}>
+          {!friend_id ? <CreatePost /> : ''}
+          <ListPost listPost={listPost} isLoading={isLoading} />
+        </Col>
+      </Row>
+    </Card.Body>
   );
 };
