7644038ebb685edb0218861974de66d31de2685d
Hoàng Anh Dũng <108377940+DungKM@users.noreply.github.com>
2023-12-02 11:26:48 +0700

    feat: search blog by major

--- resources/app/apis/services/blog.service.ts
+++ resources/app/apis/services/blog.service.ts
@@ -2,8 +2,8 @@ import { IBlogs } from '@/models/blog';
 import httpRequest from '../axios-instance';
 import { ApiConstants } from '../endpoints';
 
-const showAllBlog = <T>(page: T) => {
-  return httpRequest.get(`${ApiConstants.BLOGS}/2?page=${page}`);
+const showAllBlog = <T>(page: T, majors_id: T) => {
+  return httpRequest.get(`${ApiConstants.BLOGS}/2?page=${page}&majors_id=${majors_id}`);
 };
 const showDetailBlog = <T>(id: T) => {
   return httpRequest.get(`${ApiConstants.BLOG_DETAIL}/${id}`);
