77c9069cbef95ef2e29cf2e3942c2f5d2fc9d726
Phan Luan <phanluandev@gmail.com>
2023-12-05 01:31:18 +0700

    feat: show amount noti not seen

--- resources/app/layouts/auth/components/sidebar/single-navbar-item.tsx
+++ resources/app/layouts/auth/components/sidebar/single-navbar-item.tsx
@@ -1,3 +1,4 @@
+import { useCountNotificationsNotSeen } from '@/hooks/useNotificationQuery';
 import { Nav, OverlayTrigger, Tooltip } from 'react-bootstrap';
 import { Link, useLocation } from 'react-router-dom';
 
@@ -9,6 +10,8 @@ type SingleNavbarItemProps = {
 
 export const SingleNavbarItem = ({ title, icon, pathname }: SingleNavbarItemProps) => {
   const location = useLocation();
+  const countNotificationsNotSeen = useCountNotificationsNotSeen();
+  const checkAmountNoti = countNotificationsNotSeen && countNotificationsNotSeen > 0;
   // return (
   //   <li className={`${location.pathname === pathname ? 'active' : ''} nav-item `}>
   //     <Link className={`${location.pathname === pathname ? 'active' : ''} nav-link `} aria-current="page" to={pathname}>
@@ -26,6 +29,15 @@ export const SingleNavbarItem = ({ title, icon, pathname }: SingleNavbarItemProp
           {icon}
         </OverlayTrigger>
         <span className="item-name">{title}</span>
+        {title === 'Thông báo' && (
+          <span
+            className={`text-default ${
+              checkAmountNoti ? 'text-primary border border-primary rounded-circle px-1' : ''
+            }`}
+          >
+            {checkAmountNoti && countNotificationsNotSeen}
+          </span>
+        )}
       </Link>
     </Nav.Item>
   );
