c8d4247739826d35ea065e680dba3b50b3ad30ad
Nguyen Thanh Tung <tung19022003@gmail.com>
2023-12-06 04:32:36 +0700

    feat: activity for user when active or unactive

---
+++ app/Listeners/UserLoggedIn.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Listeners;
+
+use Illuminate\Auth\Events\Login;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Queue\InteractsWithQueue;
+
+class UserLoggedIn
+{
+    /**
+     * Create the event listener.
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Handle the event.
+     */
+    public function handle(Login $event)
+    {
+        $user = $event->user;
+        $user->update(['activity_user' => 'Online']);
+    }
+}
